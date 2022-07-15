e041e7ce504f5514d350e1751ea518f0539757ab
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-15 19:12:35 +0200

    ✨ Add rayDiv function
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- test/TestWadRayMath.sol
+++ test/TestWadRayMath.sol
@@ -16,9 +16,20 @@ contract WadRayMathFunctions {
     function refRayMul(uint256 x, uint256 y) public pure returns (uint256) {
         return RefWadRayMath.rayMul(x, y);
     }
+
+    function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayDiv(x, y);
+    }
+
+    function refRayDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return RefWadRayMath.rayDiv(x, y);
+    }
 }
 
 contract TestWadRayMath is Test {
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant RAY = 1e27;
+
     WadRayMathFunctions math;
 
     function setUp() public {
@@ -31,11 +42,26 @@ contract TestWadRayMath is Test {
         assertEq(WadRayMath.rayMul(x, y), RefWadRayMath.rayMul(x, y));
     }
 
+    function testRayDiv(uint128 _x, uint128 _y) public {
+        vm.assume(_y > 0);
+        uint256 x = _x;
+        uint256 y = _y;
+        assertEq(WadRayMath.rayDiv(x, y), RefWadRayMath.rayDiv(x, y));
+    }
+
     function testGasRayMul() public view {
-        math.rayMul(10, 10);
+        math.rayMul(2 * RAY, RAY);
     }
 
     function testGasRefRayMul() public view {
-        math.refRayMul(10, 10);
+        math.refRayMul(2 * RAY, RAY);
+    }
+
+    function testGasRayDiv() public view {
+        math.rayDiv(10 * RAY, RAY);
+    }
+
+    function testGasRefRayDiv() public view {
+        math.refRayDiv(10 * RAY, RAY);
     }
 }
