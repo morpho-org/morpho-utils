7c1bdf63699a4d3945b4ac0ab4e7d0b699d04faf
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-17 14:32:12 +0200

    ✅ Test gas for compound math
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- test/TestCompoundMath.sol
+++ test/TestCompoundMath.sol
@@ -3,14 +3,25 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "src/CompoundMath.sol";
+import "./references/CompoundMathRef.sol";
 
 contract TestCompoundMath is Test {
-    using CompoundMath for uint256;
+    uint256 internal constant WAD = 1e18;
+
+    CompoundMathFunctions compoundMath;
+    CompoundMathFunctionsRef compoundMathRef;
+
+    function setUp() public {
+        compoundMath = new CompoundMathFunctions();
+        compoundMathRef = new CompoundMathFunctionsRef();
+    }
+
+    /// TESTS ///
 
     function testMul(uint128 _x, uint128 _y) public {
         uint256 x = _x;
         uint256 y = _y;
-        assertEq(x.mul(y), mul(x, y));
+        assertEq(compoundMath.mul(x, y), compoundMathRef.mul(x, y));
     }
 
     function testDiv(uint128 _x, uint128 _y) public {
@@ -18,16 +29,38 @@ contract TestCompoundMath is Test {
 
         uint256 x = _x;
         uint256 y = _y;
-        assertEq(x.div(y), div(x, y));
+        assertEq(compoundMath.div(x, y), compoundMathRef.div(x, y));
     }
 
-    /// REFERENCES ///
+    /// GAS COMPARISONS ///
 
-    function mul(uint256 x, uint256 y) internal pure returns (uint256) {
-        return (x * y) / 1e18;
+    function testGasMul() public view {
+        compoundMath.mul(2 * WAD, WAD);
+        compoundMathRef.mul(2 * WAD, WAD);
     }
 
-    function div(uint256 x, uint256 y) internal pure returns (uint256) {
-        return ((1e18 * x * 1e18) / y) / 1e18;
+    function testGasDiv() public view {
+        compoundMath.div(10 * WAD, WAD);
+        compoundMathRef.div(10 * WAD, WAD);
+    }
+}
+
+contract CompoundMathFunctions {
+    function mul(uint256 x, uint256 y) public pure returns (uint256) {
+        return CompoundMath.mul(x, y);
+    }
+
+    function div(uint256 x, uint256 y) public pure returns (uint256) {
+        return CompoundMath.div(x, y);
+    }
+}
+
+contract CompoundMathFunctionsRef {
+    function mul(uint256 x, uint256 y) public pure returns (uint256) {
+        return CompoundMathRef.mul(x, y);
+    }
+
+    function div(uint256 x, uint256 y) public pure returns (uint256) {
+        return CompoundMathRef.div(x, y);
     }
 }
