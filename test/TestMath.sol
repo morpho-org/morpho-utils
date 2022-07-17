8c01e807cadee8b0e52e8cae6364f34396f614f2
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-17 14:41:21 +0200

    ✅ Test gas math functions
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- test/TestMath.sol
+++ test/TestMath.sol
@@ -6,31 +6,57 @@ import "src/Math.sol";
 import "./references/MathRef.sol";
 
 contract TestMath is Test {
+    MathFunctions math;
+    MathFunctionsRef mathRef;
+
+    function setUp() public {
+        math = new MathFunctions();
+        mathRef = new MathFunctionsRef();
+    }
+
+    /// TESTS ///
+
     function testMin(uint256 x, uint256 y) public {
-        assertEq(Math.min(x, y), MathRef.min(x, y));
+        assertEq(math.min(x, y), mathRef.min(x, y));
     }
 
     function testMax(uint256 x, uint256 y) public {
-        assertEq(Math.max(x, y), MathRef.max(x, y));
+        assertEq(math.max(x, y), mathRef.max(x, y));
     }
 
-    function testGasMin() public pure {
-        Math.min(1, 2);
-        Math.min(2, 1);
+    // GAS COMPARISONS ///
+
+    function testGasMin() public view {
+        math.min(1, 2);
+        math.min(2, 1);
+        mathRef.min(1, 2);
+        mathRef.min(2, 1);
     }
 
-    function testGasMinRef() public pure {
-        MathRef.min(1, 2);
-        MathRef.min(2, 1);
+    function testGasMax() public view {
+        math.max(1, 2);
+        math.max(2, 1);
+        mathRef.max(1, 2);
+        mathRef.max(2, 1);
+    }
+}
+
+contract MathFunctions {
+    function min(uint256 x, uint256 y) public pure returns (uint256) {
+        return Math.min(x, y);
     }
 
-    function testGasMax() public pure {
-        Math.max(1, 2);
-        Math.max(2, 1);
+    function max(uint256 x, uint256 y) public pure returns (uint256) {
+        return Math.max(x, y);
+    }
+}
+
+contract MathFunctionsRef {
+    function min(uint256 x, uint256 y) public pure returns (uint256) {
+        return MathRef.min(x, y);
     }
 
-    function testGasMaxRef() public pure {
-        MathRef.max(1, 2);
-        MathRef.max(2, 1);
+    function max(uint256 x, uint256 y) public pure returns (uint256) {
+        return MathRef.max(x, y);
     }
 }
