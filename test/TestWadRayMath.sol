153cd8e1af0a8a3c153d7040d42a51856233e54a
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-22 15:30:12 +0200

    🧑‍💻 Move helpers contracts upward
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- test/TestWadRayMath.sol
+++ test/TestWadRayMath.sol
@@ -7,6 +7,58 @@ import "forge-std/Vm.sol";
 import "src/WadRayMath.sol";
 import {WadRayMath as WadRayMathRef} from "@aave/core-v3/contracts/protocol/libraries/math/WadRayMath.sol";
 
+contract WadRayMathFunctions {
+    function rayMul(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayMul(x, y);
+    }
+
+    function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayDiv(x, y);
+    }
+
+    function wadMul(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadMul(x, y);
+    }
+
+    function wadDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadDiv(x, y);
+    }
+
+    function rayToWad(uint256 x) public pure returns (uint256) {
+        return WadRayMath.rayToWad(x);
+    }
+
+    function wadToRay(uint256 x) public pure returns (uint256) {
+        return WadRayMath.wadToRay(x);
+    }
+}
+
+contract WadRayMathFunctionsRef {
+    function rayMul(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMathRef.rayMul(x, y);
+    }
+
+    function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMathRef.rayDiv(x, y);
+    }
+
+    function wadMul(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMathRef.wadMul(x, y);
+    }
+
+    function wadDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMathRef.wadDiv(x, y);
+    }
+
+    function rayToWad(uint256 x) public pure returns (uint256) {
+        return WadRayMathRef.rayToWad(x);
+    }
+
+    function wadToRay(uint256 x) public pure returns (uint256) {
+        return WadRayMathRef.wadToRay(x);
+    }
+}
+
 contract TestWadRayMath is Test {
     uint256 internal constant WAD = 1e18;
     uint256 internal constant RAY = 1e27;
@@ -87,59 +139,5 @@ contract TestWadRayMath is Test {
     function testGasWadToRay() public view {
         math.wadToRay(2 * WAD);
         mathRef.wadToRay(2 * WAD);
-    }
-}
-
-/// Needed for gas report ///
-
-contract WadRayMathFunctions {
-    function rayMul(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMath.rayMul(x, y);
-    }
-
-    function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMath.rayDiv(x, y);
-    }
-
-    function wadMul(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMath.wadMul(x, y);
-    }
-
-    function wadDiv(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMath.wadDiv(x, y);
-    }
-
-    function rayToWad(uint256 x) public pure returns (uint256) {
-        return WadRayMath.rayToWad(x);
-    }
-
-    function wadToRay(uint256 x) public pure returns (uint256) {
-        return WadRayMath.wadToRay(x);
-    }
-}
-
-contract WadRayMathFunctionsRef {
-    function rayMul(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMathRef.rayMul(x, y);
-    }
-
-    function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMathRef.rayDiv(x, y);
-    }
-
-    function wadMul(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMathRef.wadMul(x, y);
-    }
-
-    function wadDiv(uint256 x, uint256 y) public pure returns (uint256) {
-        return WadRayMathRef.wadDiv(x, y);
-    }
-
-    function rayToWad(uint256 x) public pure returns (uint256) {
-        return WadRayMathRef.rayToWad(x);
-    }
-
-    function wadToRay(uint256 x) public pure returns (uint256) {
-        return WadRayMathRef.wadToRay(x);
     }
 }
