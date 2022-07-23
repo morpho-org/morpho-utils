f25831242f5f98661c14c2e7aab22c7fc9fbf718
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-23 17:09:52 +0200

    ⚡️ Come back to simple and more efficient functions
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- src/Math.sol
+++ src/Math.sol
@@ -2,28 +2,12 @@
 pragma solidity ^0.8.0;
 
 library Math {
-    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        assembly {
-            switch lt(x, y)
-            case 0 {
-                z := y
-            }
-            default {
-                z := x
-            }
-        }
+    function min(uint256 x, uint256 y) internal pure returns (uint256) {
+        return x < y ? x : y;
     }
 
-    function max(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        assembly {
-            switch gt(x, y)
-            case 0 {
-                z := y
-            }
-            default {
-                z := x
-            }
-        }
+    function max(uint256 x, uint256 y) internal pure returns (uint256) {
+        return x > y ? x : y;
     }
 
     function zeroFloorSub(uint256 x, uint256 y) internal pure returns (uint256 z) {
