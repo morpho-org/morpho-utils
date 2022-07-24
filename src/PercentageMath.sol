de31a2db2ecf333974b0ea50ff9c5d7898ceb59f
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-24 11:45:43 +0200

    ⚡️ Simplify code
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- src/PercentageMath.sol
+++ src/PercentageMath.sol
@@ -25,7 +25,7 @@ library PercentageMath {
         // <=> x * percentage > type(uint256).max - HALF_PERCENTAGE_FACTOR
         // <=> x > (type(uint256).max - HALF_PERCENTAGE_FACTOR) / percentage
         assembly {
-            if and(iszero(iszero(percentage)), gt(x, div(MAX_UINT256_MINUS_HALF_PERCENTAGE, percentage))) {
+            if and(gt(percentage, 0), gt(x, div(MAX_UINT256_MINUS_HALF_PERCENTAGE, percentage))) {
                 revert(0, 0)
             }
 
@@ -43,7 +43,7 @@ library PercentageMath {
         // <=> x * PERCENTAGE_FACTOR > type(uint256).max - halfPercentage
         // <=> x > type(uint256).max - halfPercentage / PERCENTAGE_FACTOR
         assembly {
-            if and(iszero(iszero(percentage)), gt(x, div(sub(MAX_UINT256, div(percentage, 2)), PERCENTAGE_FACTOR))) {
+            if and(gt(percentage, 0), gt(x, div(sub(MAX_UINT256, div(percentage, 2)), PERCENTAGE_FACTOR))) {
                 revert(0, 0)
             }
 
