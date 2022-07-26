d767c84f5b62088d9610599bfae62a70357b59e9
AtomicAzzaz <paul@dufresne.fr>
2022-07-26 15:20:04 +0200

    ⚡️ Save gas
    delete unused lib

--- src/DelegateCall.sol
+++ src/DelegateCall.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.0;
 
 /// @title Delegate Call Library.
-/// @author Morpho Labs.
+/// @author Morpho Labs - MEP.
 /// @custom:contact security@morpho.xyz
-/// @dev Library to perform delegate calls inspired by the OZ Address library: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol.
+/// @dev Low-level YUL delegate call library
 library DelegateCall {
     /// ERRORS ///
 
     /// @notice Thrown when a low delegate call has failed without error message.
     error LowLevelDelegateCallFailed();
+    bytes4 constant LowLevelDelegateCallFailedError = 0x06f7035e; // bytes4(keccak256("LowLevelDelegateCallFailed()"))
 
     /// INTERNAL ///
 
@@ -17,18 +18,29 @@ library DelegateCall {
     /// @dev Note: Unlike the OZ's library this function does not check if the `_target` is a contract. It is the responsibility of the caller to ensure that the `_target` is a contract.
     /// @param _target The address of the target contract.
     /// @param _data The data to pass to the function called on the target contract.
-    /// @return The return data from the function called on the target contract.
-    function functionDelegateCall(address _target, bytes memory _data) internal returns (bytes memory) {
-        (bool success, bytes memory returndata) = _target.delegatecall(_data);
-        if (success) return returndata;
-        else {
-            // Look for revert reason and bubble it up if present.
-            if (returndata.length > 0) {
-                // The easiest way to bubble the revert reason is using memory via assembly.
-                assembly {
-                    revert(add(32, returndata), mload(returndata))
+    /// @return _returnData The return data from the function called on the target contract.
+    function functionDelegateCall(address _target, bytes memory _data) internal returns (bytes memory _returnData) {
+ 
+        assembly {
+            // the bytes size is found at the bytes pointer memory address - the bytes data is found a slot further
+            let result := delegatecall(gas(), _target, add(_data, 0x20), mload(_data), 0, 0)  
+            let size := returndatasize()
+            
+            mstore(_returnData, size)
+            returndatacopy(add(_returnData, 0x20), 0, size)
+
+            if iszero(result){
+                if iszero(size){
+                        mstore(_returnData, LowLevelDelegateCallFailedError)
+                        revert(_returnData, 4)
+                    }
+                revert(add(_returnData, 0x20), size)
                 }
-            } else revert LowLevelDelegateCallFailed();
+
+            //update the free memory pointer
+            mstore(0x40, add(add(_returnData, 0x20), size)) 
+ 
         }
+
     }
 }
