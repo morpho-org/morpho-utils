a3cb3512b4dacbd777454a405fb0f023b3ee44ce
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-27 10:28:04 +0200

    📝 Add doc for compound math

--- src/CompoundMath.sol
+++ src/CompoundMath.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
+/// @title Compound Math Library.
+/// @author Morpho Labs.
+/// @custom:contact security@morpho.xyz
+/// @dev Library to perform Compound's multiplication and division in an efficient way.
 library CompoundMath {
+    /// CONSTANTS ///
+
     uint256 public constant SCALE = 1e36;
     uint256 public constant WAD = 1e18;
+
+    /// INTERNAL ///
 
     function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         assembly {
