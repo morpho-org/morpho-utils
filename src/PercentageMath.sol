15e15ea1162481b1a05f5a6461a60790cdca7a9b
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-27 11:01:02 +0200

    ⚡️ Cheaper and clear div version

--- src/PercentageMath.sol
+++ src/PercentageMath.sol
@@ -43,9 +43,7 @@ library PercentageMath {
         // <=> x * PERCENTAGE_FACTOR > type(uint256).max - halfPercentage
         // <=> x > type(uint256).max - halfPercentage / PERCENTAGE_FACTOR
         assembly {
-            if iszero(
-                and(gt(percentage, 0), iszero(gt(x, div(sub(MAX_UINT256, div(percentage, 2)), PERCENTAGE_FACTOR))))
-            ) {
+            if or(iszero(percentage), gt(x, div(sub(MAX_UINT256, div(percentage, 2)), PERCENTAGE_FACTOR))) {
                 revert(0, 0)
             }
 
