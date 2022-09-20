d15c28b0bef5ea5dc747ae79c065f4255731abc1
Rubilmax <rmilon@gmail.com>
2022-09-20 12:09:32 +0200

    feat(ts): added maths npm package

---
+++ ts/maths/WadRayMath.ts
@@ -0,0 +1,16 @@
+import { mulDivUp } from "./utils";
+import { BigNumber, BigNumberish } from "ethers";
+
+export const WAD = BigNumber.from(10).pow(18);
+export const RAY = BigNumber.from(10).pow(27);
+
+const WadRayMath = {
+  WAD,
+  RAY,
+  wadMul: (x: BigNumberish, y: BigNumberish) => mulDivUp(x, y, WAD),
+  wadDiv: (x: BigNumberish, y: BigNumberish) => mulDivUp(x, WAD, y),
+  rayMul: (x: BigNumberish, y: BigNumberish) => mulDivUp(x, y, RAY),
+  rayDiv: (x: BigNumberish, y: BigNumberish) => mulDivUp(x, RAY, y),
+};
+
+export default WadRayMath;
