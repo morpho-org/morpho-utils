9719f5d927f85165e44dd83fdfd2ed8cae8bb3c5
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-11 19:47:23 +0100

    feat: add cast uint 128, 64, 32, 16, 8

--- src/math/SafeCast.sol
+++ src/math/SafeCast.sol
@@ -7,11 +7,66 @@ pragma solidity ^0.8.0;
 /// @notice TODO
 library SafeCast {
 
+	uint256 internal constant MAX_UINT128 = 2**128 - 1; // Not possible to use type(uint128).max in yul
 	uint256 internal constant MAX_UINT96 = 2**96 - 1; // Not possible to use type(uint96).max in yul
+	uint256 internal constant MAX_UINT64 = 2**64 - 1; // Not possible to use type(uint64).max in yul
+	uint256 internal constant MAX_UINT32 = 2**32 - 1; // Not possible to use type(uint32).max in yul
+	uint256 internal constant MAX_UINT16 = 2**16 - 1; // Not possible to use type(uint16).max in yul
+	uint256 internal constant MAX_UINT8 = 2**8 - 1; // Not possible to use type(uint8).max in yul
+
+	function toUint128(uint256 value) internal pure returns(uint128 casted) {
+		assembly {
+			if iszero(lt(value, MAX_UINT128)) {
+				revert(0, 0)
+			}
+
+			casted := value
+		}
+	}
 
 	function toUint96(uint256 value) internal pure returns(uint96 casted) {
 		assembly {
 			if iszero(lt(value, MAX_UINT96)) {
+				revert(0, 0)
+			}
+
+			casted := value
+		}
+	}
+
+	function toUint64(uint256 value) internal pure returns(uint64 casted) {
+		assembly {
+			if iszero(lt(value, MAX_UINT64)) {
+				revert(0, 0)
+			}
+
+			casted := value
+		}
+	}
+
+	function toUint32(uint256 value) internal pure returns(uint32 casted) {
+		assembly {
+			if iszero(lt(value, MAX_UINT32)) {
+				revert(0, 0)
+			}
+
+			casted := value
+		}
+	}
+
+	function toUint16(uint256 value) internal pure returns(uint16 casted) {
+		assembly {
+			if iszero(lt(value, MAX_UINT16)) {
+				revert(0, 0)
+			}
+
+			casted := value
+		}
+	}
+
+	function toUint8(uint256 value) internal pure returns(uint8 casted) {
+		assembly {
+			if iszero(lt(value, MAX_UINT8)) {
 				revert(0, 0)
 			}
 
--- test/TestSafeCast.sol
+++ test/TestSafeCast.sol
@@ -29,18 +29,84 @@ contract TestSafeCast is Test {
 
     /// TESTS ///
 
-    function testToUint96Overflow(uint256 x) public {
+    function testOverflowToUint128(uint256 x) public {
+        vm.assume(x > type(uint128).max);
+
+        vm.expectRevert();
+        SafeCast.toUint128(x);
+    }
+
+    function testCastToUint128(uint256 x) public {
+        vm.assume(x <= type(uint96).max);
+
+        assertEq(SafeCast.toUint128(x), uint128(x));
+    }
+
+    function testOverflowToUint96(uint256 x) public {
         vm.assume(x > type(uint96).max);
 
         vm.expectRevert();
-        safeCast.toUint96(x);
+        SafeCast.toUint96(x);
     }
 
     function testCastToUint96(uint256 x) public {
         vm.assume(x <= type(uint96).max);
 
-        assertEq(safeCast.toUint96(x), uint96(x));
+        assertEq(SafeCast.toUint96(x), uint96(x));
     }
+
+    function testOverflowToUint64(uint256 x) public {
+        vm.assume(x > type(uint64).max);
+
+        vm.expectRevert();
+        SafeCast.toUint64(x);
+    }
+
+    function testCastToUint64(uint256 x) public {
+        vm.assume(x <= type(uint64).max);
+
+        assertEq(SafeCast.toUint64(x), uint64(x));
+    }
+
+    function testCastToUint32(uint256 x) public {
+        vm.assume(x <= type(uint32).max);
+
+        assertEq(SafeCast.toUint32(x), uint32(x));
+    }
+
+    function testOverflowToUint32(uint256 x) public {
+        vm.assume(x > type(uint32).max);
+
+        vm.expectRevert();
+        SafeCast.toUint32(x);
+    }
+
+    function testCastToUint16(uint256 x) public {
+        vm.assume(x <= type(uint16).max);
+
+        assertEq(SafeCast.toUint16(x), uint16(x));
+    }
+
+    function testOverflowToUint16(uint256 x) public {
+        vm.assume(x > type(uint16).max);
+
+        vm.expectRevert();
+        SafeCast.toUint16(x);
+    }
+
+    function testCastToUint8(uint256 x) public {
+        vm.assume(x <= type(uint8).max);
+
+        assertEq(SafeCast.toUint8(x), uint8(x));
+    }
+
+    function testOverflowToUint8(uint256 x) public {
+        vm.assume(x > type(uint8).max);
+
+        vm.expectRevert();
+        SafeCast.toUint8(x);
+    }
+
 
     // GAS COMPARISONS ///
 
