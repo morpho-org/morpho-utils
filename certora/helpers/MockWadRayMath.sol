b0c04a7ae03c1e46c3a4cb4bc2f4495f18ed7d15
Rubilmax <rmilon@gmail.com>
2022-12-15 10:37:02 +0100

    Add certora formal proofs

--- certora/helpers/MockWadRayMath.sol
+++ certora/helpers/MockWadRayMath.sol
@@ -8,16 +8,48 @@ contract MockWadRayMath {
         return WadRayMath.wadMul(x, y);
     }
 
+    function wadMulDown(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadMulDown(x, y);
+    }
+
+    function wadMulUp(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadMulUp(x, y);
+    }
+
     function wadDiv(uint256 x, uint256 y) public pure returns (uint256) {
         return WadRayMath.wadDiv(x, y);
+    }
+
+    function wadDivDown(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadDivDown(x, y);
+    }
+
+    function wadDivUp(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.wadDivUp(x, y);
     }
 
     function rayMul(uint256 x, uint256 y) public pure returns (uint256) {
         return WadRayMath.rayMul(x, y);
     }
 
+    function rayMulDown(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayMulDown(x, y);
+    }
+
+    function rayMulUp(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayMulUp(x, y);
+    }
+
     function rayDiv(uint256 x, uint256 y) public pure returns (uint256) {
         return WadRayMath.rayDiv(x, y);
+    }
+
+    function rayDivDown(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayDivDown(x, y);
+    }
+
+    function rayDivUp(uint256 x, uint256 y) public pure returns (uint256) {
+        return WadRayMath.rayDivUp(x, y);
     }
 
     function rayToWad(uint256 x) public pure returns (uint256) {
