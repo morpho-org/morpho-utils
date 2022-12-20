18dcce766da57854364dfbb762d7238362b4c3b8
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-12-20 15:53:03 +0100

    fix: do not return

--- src/math/SafeCast.sol
+++ src/math/SafeCast.sol
@@ -6,80 +6,76 @@ pragma solidity ^0.8.0;
 /// @custom:contact security@morpho.xyz
 /// @notice TODO
 library SafeCast {
-    function toUint128(uint256 value) internal pure returns (uint128) {
+    function toUint128(uint256 value) internal pure returns (uint128 casted) {
         assembly {
-            if lt(value, shl(128, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(128, 1))) {
+				revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
         }
     }
 
-    function toUint96(uint256 value) internal pure returns (uint96) {
+    function toUint96(uint256 value) internal pure returns (uint96 casted) {
         assembly {
-            if lt(value, shl(96, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(96, 1))) {
+            	revert(0, 0)
             }
-
-            revert(0, 0)
+			
+			casted := value
         }
     }
 
-    function toUint64(uint256 value) internal pure returns (uint64) {
+    function toUint64(uint256 value) internal pure returns (uint64 casted) {
         assembly {
-            if lt(value, shl(64, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(64, 1))) {
+            	revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
+
         }
     }
 
-    function toUint32(uint256 value) internal pure returns (uint32) {
+    function toUint32(uint256 value) internal pure returns (uint32 casted) {
         assembly {
-            if lt(value, shl(32, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(32, 1))) {
+            	revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
+
         }
     }
 
-    function toUint16(uint256 value) internal pure returns (uint16) {
+    function toUint16(uint256 value) internal pure returns (uint16 casted) {
         assembly {
-            if lt(value, shl(16, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(16, 1))) {
+            	revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
+
         }
     }
 
-    function toUint8(uint256 value) internal pure returns (uint8) {
+    function toUint8(uint256 value) internal pure returns (uint8 casted) {
         assembly {
-            if lt(value, shl(8, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(8, 1))) {
+            	revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
         }
     }
 
-	function toInt128(int256 value) internal pure returns (int128) {
+	function toInt128(int256 value) internal pure returns (int128 casted) {
 		assembly {
-            if lt(value, shl(127, 1)) {
-                mstore(0x00, value)
-                return(0x00, 0x20)
+            if iszero(lt(value, shl(127, 1))) {
+            	revert(0, 0)
             }
 
-            revert(0, 0)
+			casted := value
         }
 	}
 }
