71a80238791a7ac552efd2c454442fc2f7f39b3b
Quentin Garchery <garchery.quentin@gmail.com>
2022-12-22 13:40:41 +0100

    docs: small refactor & add comments

--- test/helpers/DeBruijn.sol
+++ test/helpers/DeBruijn.sol
@@ -3,32 +3,31 @@ pragma solidity ^0.8.0;
 
 library DeBruijn {
     function isDeBruijnSequence(uint256 seq) public pure returns (bool) {
-        uint256 shift = 2**248;
-
         bool[] memory seen = new bool[](256);
 
         for (uint256 i; i < 256; i++) {
             uint256 x = 2**i;
-            uint256 frame;
+            uint256 key;
+            // Inline assembly to avoid overflow
             assembly {
-                frame := div(mul(x, seq), shift)
+                key := shr(248, mul(x, seq))
             }
-            if (seen[frame]) return false;
-            seen[frame] = true;
+            if (seen[key]) return false;
+            seen[key] = true;
         }
         return true;
     }
 
     function precomputeRows(uint256 seq) public pure returns (uint256[] memory) {
-        uint256 shift = 2**248;
         uint256[] memory rows = new uint256[](8);
 
         for (uint256 i; i < 256; i++) {
             uint256 x = 2**i;
 
             uint256 key;
+            // Inline assembly to avoid overflow
             assembly {
-                key := div(mul(x, seq), shift)
+                key := shr(248, mul(x, seq))
             }
             uint256 rowIndex = key / 32;
             uint256 colIndex = key % 32;
