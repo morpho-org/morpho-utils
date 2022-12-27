de956993bd9535216e783ee9e44109068613664e
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-27 17:57:35 +0100

    perf: more efficient context

--- src/meta-tx/ContextMixin.sol
+++ src/meta-tx/ContextMixin.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.0;
 abstract contract ContextMixin {
     function _msgSender() internal view returns (address sender) {
         if (msg.sender == address(this)) {
-            bytes memory array = msg.data;
-            uint256 index = msg.data.length;
             assembly {
-                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
-                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+                sender := shr(96, calldataload(sub(calldatasize(), 20)))
             }
         } else sender = msg.sender;
     }
