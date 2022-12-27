f9184b77f0c123aa55e6c230c0854ebbb1d42cde
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-27 15:40:05 +0100

    feat: add ContextMixin file

---
+++ src/meta-tx/ContextMixin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity ^0.8.0;
+
+abstract contract ContextMixin {
+    function _msgSender() internal view returns (address sender) {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else sender = msg.sender;
+    }
+}
