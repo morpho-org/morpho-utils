5351a77ac0d3168fa47c0b042983b15203a10b26
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-07 01:45:31 +0100

    perf: shave a multiplication

--- src/math/Math.sol
+++ src/math/Math.sol
@@ -26,9 +26,7 @@ library Math {
         // Division by 0 if
         //    y = 0
         assembly {
-            if iszero(y) {
-                revert(0, 0)
-            }
+            if iszero(y) { revert(0, 0) }
 
             z := add(gt(mod(x, y), 0), div(x, y))
         }
@@ -55,11 +53,8 @@ library Math {
             x := or(x, shr(8, x))
             x := or(x, shr(16, x))
             let deBruijnSeq := 0x07c4acdd
-            let offset := shr(251, shl(224, mul(deBruijnSeq, x)))
-            y := add(
-                y,
-                shr(248, shl(mul(8, offset), 0x0009010a0d15021d0b0e10121619031e080c141c0f111807131b17061a05041f))
-            )
+            let offset := shr(24, and(0xf8000000, mul(deBruijnSeq, x)))
+            y := add(y, shr(248, shl(offset, 0x0009010a0d15021d0b0e10121619031e080c141c0f111807131b17061a05041f)))
         }
     }
 }
